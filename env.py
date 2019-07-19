7dd673bad87bc7fcea442a3698b2001e15e25071
gerome12 <gerome.pasquier@gmail.com>
2019-07-19 17:14:33 +0200

    Update env.py

--- env.py
+++ env.py
@@ -1,6 +1,8 @@
 
-__URL="http://localhost:8042/api"
-#__URL="https://test2.kheops.online/api"
+import os
+
+__TRAVIS_URL="http://localhost:8042/api"
+__TEST2_URL="https://test2.kheops.online/api"
 #__URL="http://172.17.20.103:8042/api"
 __USER_1_TOKEN = "eyJhbGciOiJIUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJhdXRob3JpemF0aW9uLmtoZW9wcy5vbmxpbmUiLCJzdWIiOiJjMmZkMGI2Ni1kYWM0LTRiOTgtOTM0MS1kNGYzYjMwM2JkNGUiLCJlbWFpbCI6InRpdGlAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MjUyMjk0MzU3MiwiaXNzIjoiYXV0aG9yaXphdGlvbi5raGVvcHMub25saW5lIiwiaWF0IjoxNTIyOTM5OTcyfQ.hrau4sW3iVf_mS0gLiO_uulMrNUTe_D9FRKfK6AtK48"
 __STUDY_UID = "2.16.840.1.113669.632.20.1211.10000314223"
@@ -9,7 +11,10 @@ __PRINT_INFO = False
 
 def initialize():
     global URL
-    URL = __URL
+    if "TRAVIS" in os.environ:
+        URL = __TRAVIS_URL
+    else:
+        URL = __TEST2_URL
     global USER_1_TOKEN
     USER_1_TOKEN = __USER_1_TOKEN
     global SERIES_UID
