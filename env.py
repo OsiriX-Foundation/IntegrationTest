20601c221794b65c949aeabe0e39967051969ec9
gerome12 <gerome.pasquier@gmail.com>
2019-07-19 17:50:58 +0200

    .

--- env.py
+++ env.py
@@ -2,8 +2,8 @@
 import os
 
 __TRAVIS_URL="http://localhost:8042/api"
-__TEST2_URL="https://test2.kheops.online/api"
-#__URL="http://172.17.20.103:8042/api"
+#__TEST2_URL="https://test2.kheops.online/api"
+__TEST2_URL="http://172.17.20.103:8042/api"
 __USER_1_TOKEN = "eyJhbGciOiJIUzI1NiIsImtpZCI6IjEifQ.eyJhdWQiOiJhdXRob3JpemF0aW9uLmtoZW9wcy5vbmxpbmUiLCJzdWIiOiJjMmZkMGI2Ni1kYWM0LTRiOTgtOTM0MS1kNGYzYjMwM2JkNGUiLCJlbWFpbCI6InRpdGlAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MjUyMjk0MzU3MiwiaXNzIjoiYXV0aG9yaXphdGlvbi5raGVvcHMub25saW5lIiwiaWF0IjoxNTIyOTM5OTcyfQ.hrau4sW3iVf_mS0gLiO_uulMrNUTe_D9FRKfK6AtK48"
 __STUDY_UID = "2.16.840.1.113669.632.20.1211.10000314223"
 __SERIES_UID = "1.3.12.2.1107.5.1.4.48545.30000006100307470634300004693"
