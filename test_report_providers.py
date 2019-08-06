2f8ad7d01f057b5c01d95d7a1820c6136b31a8d3
gerome12 <gerome.pasquier@gmail.com>
2019-08-06 17:14:30 +0200

    new test

--- test_report_providers.py
+++ test_report_providers.py
@@ -10,11 +10,31 @@ import util
 def test_new_album():
     print()
     album=util.new_album(token=env.env_var.get("USER_1_TOKEN"))
+    env.env_var["ALBUM_ID"]=album["album_id"]
 
 def test_stow():
     print()
-    util.stow(token=env.env_var.get("USER_1_TOKEN"))
+    params = {"album": env.env_var.get("ALBUM_ID")}
+    util.stow(token=env.env_var.get("USER_1_TOKEN"), params=params)
 
-def test_studies_list():
-    params = {"inbox": "True"}
+def test_album_studies_list():
+    params = {"album": env.env_var.get("ALBUM_ID")}
     util.studies_list(token=env.env_var.get("USER_1_TOKEN"), params=params, count=1)
+
+def test_get_report_provider_list_empty():
+    util.report_provider_list(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), count=0)
+
+def test_add_report_provider():
+    name = "report provider name"
+    url = "https://reportprovider.kheops.online/.well-known/kheops-report-configuration"
+    data = {"name": name, "url": url}
+    util.new_report_provider(token=env.env_var.get("USER_1_TOKEN"), data=data, album_id=env.env_var.get("ALBUM_ID"))
+
+def test_add_report_provider_400():
+    name = "name"
+    url = "https://reportprovider.kheops.online/.well-known/xxx"
+    data = {"name": name, "url": url}
+    util.new_report_provider(token=env.env_var.get("USER_1_TOKEN"), data=data, album_id=env.env_var.get("ALBUM_ID"), status_code=400)
+
+def test_get_report_provider_list():
+    rp = util.report_provider_list(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"))
--- util.py
+++ util.py
@@ -31,15 +31,16 @@ def stow(token, status_code = 200, file_name = "testStudy.dcm", params = {}):
     print_request("POST", response, request_url)
     assert response.status_code == status_code
 
-def new_album(token, data={"name":"a name"}):
+def new_album(token, data={"name":"a name"}, status_code=201):
     print()
     request_url = env.env_var.get("URL") + "/albums"
     headers = {"Authorization": "Bearer "+ token, "Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"}
     response = requests.post(request_url, headers=headers, data=urlencode(data))
     print_request("POST", response, request_url)
-    assert response.status_code == 201
-    album = json.loads(response.content)
-    return album
+    assert response.status_code == status_code
+    if status_code == 201:
+        album = json.loads(response.content)
+        return album
 
 def studies_list(token, params={}, count=1):
     print()
@@ -66,3 +67,29 @@ def get_token(username, password, realm="travis", client_id="loginConnect"):
     response = requests.post(token_endpoint, headers=headers, data=data)
     token_response = json.loads(response.content)
     return token_response["access_token"]
+
+################################################################
+# REPORT PROVIDER
+################################################################
+
+def new_report_provider(token, data, album_id, status_code=201):
+    print()
+    request_url = env.env_var.get("URL") + "/albums/"+str(album_id)+"/reportproviders"
+    headers = {"Authorization": "Bearer "+ token, "Content-Type": "application/x-www-form-urlencoded"}
+    response = requests.post(request_url, headers=headers, data=urlencode(data))
+    print_request("POST", response, request_url)
+    assert response.status_code == status_code
+    if status_code == 201:
+        reportprovider = json.loads(response.content)
+        return reportprovider
+
+def report_provider_list(token, album_id, params={}, count=1):
+    print()
+    request_url = env.env_var.get("URL") + "/albums/"+str(album_id)+"/reportproviders"
+    headers = {"Authorization": "Bearer "+ token}
+    response = requests.get(request_url, headers=headers, params=params)
+    print_request("GET", response, request_url)
+    assert response.status_code == 200
+    assert response.headers.get("X-Total-Count") == str(count)
+    report_providers = json.loads(response.content)
+    return report_providers
