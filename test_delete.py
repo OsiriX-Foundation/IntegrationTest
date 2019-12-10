83b921dde9222cba4c41388d5d3c6bd6247e492a
gerome12 <gerome.pasquier@gmail.com>
2019-12-10 15:46:11 +0100

    clean docker between each test file

--- test_delete.py
+++ test_delete.py
@@ -35,17 +35,22 @@ def test_stow():
     params = {"album": env.env_var.get("ALBUM_ID")}
     util.stow(token=env.env_var.get("USER_1_TOKEN"), params=params)
 
-def test_create_capability_token():
-    data={"title": "", "scope_type": "album", "album": env.env_var.get("ALBUM_ID"), "read_permission": True, "appropriate_permission": True, "download_permission": False, "write_permission": False}
+def test_create_capability_token_read_only():
+    data={"title": "title", "scope_type": "album", "album": env.env_var.get("ALBUM_ID"), "read_permission": True, "appropriate_permission": True, "download_permission": False, "write_permission": False}
     capability_token = util.new_token(token=env.env_var.get("USER_1_TOKEN"), data=data)
-    env.env_var["CAPABILITY_TOKEN"] = capability_token["secret"]
+    env.env_var["CAPABILITY_TOKEN_READ_ONLY"] = capability_token["secret"]
 
 def test_send_with_token_to_user_3_study():
-    util.share_study_with_user(token=env.env_var.get("CAPABILITY_TOKEN"), user="3685d976-f1d6-443c-95d4-95ee4b749878", studies_UID=env.env_var.get("STUDY_UID"))
+    util.share_study_with_user(token=env.env_var.get("CAPABILITY_TOKEN_READ_ONLY"), user="3685d976-f1d6-443c-95d4-95ee4b749878", studies_UID=env.env_var.get("STUDY_UID"))
 
 def test_get_studies_list_from_album():
     params = {"album": env.env_var.get("ALBUM_ID")}
     util.studies_list(token=env.env_var.get("USER_1_TOKEN"),params=params, count=1)
 
-def test_delete_with_token_write_read():
-    util.delete_series_from_inbox(token=env.env_var.get("CAPABILITY_TOKEN"), studies_UID=env.env_var.get("STUDY_UID"), series_UID=env.env_var.get("SERIES_UID"), status_code=204)
+def test_create_capability_token_read_write():
+    data={"title": "title", "scope_type": "album", "album": env.env_var.get("ALBUM_ID"), "read_permission": True, "appropriate_permission": True, "download_permission": False, "write_permission": False}
+    capability_token = util.new_token(token=env.env_var.get("USER_1_TOKEN"), data=data)
+    env.env_var["CAPABILITY_TOKEN_READ_WRITE"] = capability_token["secret"]
+
+def test_delete_with_token_read_write():
+    util.delete_series_from_inbox(token=env.env_var.get("CAPABILITY_TOKEN_READ_WRITE"), studies_UID=env.env_var.get("STUDY_UID"), series_UID=env.env_var.get("SERIES_UID"), status_code=204)
--- test_report_providers.py
+++ test_report_providers.py
@@ -7,6 +7,19 @@ import env
 import util
 
 
+def test_init():
+    env.initialize()
+    print()
+
+def test_get_token():
+    print()
+    token = util.get_token(username="titi", password="titi")
+    env.env_var["USER_1_TOKEN"] = token
+    token = util.get_token(username="toto", password="toto")
+    env.env_var["USER_2_TOKEN"] = token
+    token = util.get_token(username="tata", password="tata")
+    env.env_var["USER_3_TOKEN"] = token
+
 def test_new_album():
     print()
     album=util.new_album(token=env.env_var.get("USER_1_TOKEN"))
