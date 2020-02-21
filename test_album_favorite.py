b24f50599abf3634ba66fa6aec2b26869aea6457
salvatore <salvatore.cicciu@gmail.com>
2020-02-21 20:03:08 +0100

    fix favortie album tests

--- test_album_favorite.py
+++ test_album_favorite.py
@@ -7,6 +7,7 @@ import time
 import pytest
 import env
 import util
+import rq_album
 
 
 def test_init():
@@ -24,19 +25,19 @@ def test_get_token():
     env.env_var["USER_3_TOKEN"] = token
 
 def test_add_album_to_favorites():
-    util.delete_all_ablums(token=env.env_var['USER_1_TOKEN'])
-    album = util.new_album(token=env.env_var['USER_1_TOKEN'], data={"name":"new album favorite"})
+    rq_album.delete_all(token=env.env_var['USER_1_TOKEN'])
+    album = rq_album.create(token=env.env_var['USER_1_TOKEN'], data={"name":"new album favorite"})
     env.env_var["ALBUM_ID_1"]=album["album_id"]
-    util.add_favorite(env.env_var.get("USER_1_TOKEN"), env.env_var["ALBUM_ID_1"])
+    rq_album.add_favorite(env.env_var.get("USER_1_TOKEN"), env.env_var["ALBUM_ID_1"])
 
 def test_get_album_list_filter_by_favorite():
-    list_albums = util.list_albums(token=env.env_var.get("USER_1_TOKEN"), params={"favorite":True}, count=1)
+    list_albums = rq_album.get_list(token=env.env_var.get("USER_1_TOKEN"), params={"favorite":True}, count=1)
     assert list_albums[0]["album_id"] == env.env_var['ALBUM_ID_1']
 
 def test_remove_album_to_favorites():
-    album = util.new_album(token=env.env_var['USER_1_TOKEN'], data={"name":"new album remove favorite"})
+    album = rq_album.create(token=env.env_var['USER_1_TOKEN'], data={"name":"new album remove favorite"})
     env.env_var["ALBUM_ID_2"]=album["album_id"]
     #add and remove
-    util.add_favorite(env.env_var.get("USER_1_TOKEN"), env.env_var["ALBUM_ID_2"])
-    util.remove_favorite(env.env_var.get("USER_1_TOKEN"), env.env_var["ALBUM_ID_2"])
-    util.list_albums(token=env.env_var.get("USER_1_TOKEN"), params={"favorite":True}, count=1)
+    rq_album.add_favorite(env.env_var.get("USER_1_TOKEN"), env.env_var["ALBUM_ID_2"])
+    rq_album.remove_favorite(env.env_var.get("USER_1_TOKEN"), env.env_var["ALBUM_ID_2"])
+    rq_album.get_list(token=env.env_var.get("USER_1_TOKEN"), params={"favorite":True}, count=1)
