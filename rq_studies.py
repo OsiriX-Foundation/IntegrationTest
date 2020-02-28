a912e8aad68bbb42568177932ab565494385b54c
salvatore <salvatore.cicciu@gmail.com>
2020-02-28 17:12:23 +0100

    updated studies tests

--- rq_studies.py
+++ rq_studies.py
@@ -24,11 +24,26 @@ def get_list(token, params={}, count=1):
         assert response.status_code == 204
         assert response.headers.get("X-Total-Count") == str(count)
 
-def stow(token, status_code = 200, file_name = "series/test1.dcm", params = {}):
+def stow(token, file_name = "series/test1.dcm", params = {}, status_code = 200):
     print()
     request_url = env.env_var.get("URL") + "/studies"
     headers = {"Authorization": "Bearer "+ token, "Content-Type": "multipart/related; type=\"application/dicom\"; boundary=myboundary"}
     files = {'file': open(file_name, 'rb')}
     response = requests.post(request_url, headers=headers, files=files, params=params)
     util.print_request("POST", response, request_url)
+    assert response.status_code == status_code
+
+def add_in_album(token, params, study_instance_uid, album_id_dest, status_code=201):
+    print()
+    request_url = env.env_var.get("URL") + "/studies/" + study_instance_uid + "/albums/" + album_id_dest
+    if("album" in params and "inbox" in params):
+        assert False #no permission
+    elif("album" in params):
+        request_url +="?album="+params["album"]
+    elif("inbox" in params):
+        request_url +="?inbox="+str(params["inbox"])
+    
+    headers = {"Authorization": "Bearer "+ token}
+    response = requests.put(request_url, headers=headers)
+    util.print_request("PUT", response, request_url)
     assert response.status_code == status_code
--- test_album_sharing.py
+++ test_album_sharing.py
@@ -20,26 +20,42 @@ def test_get_token():
     token = util.get_token(username="tata", password="tata")
     env.env_var["USER_3_TOKEN"] = token
 
-def test_new_album():
-    rq_album.delete_all(token=env.env_var['USER_1_TOKEN'])
+def test_create_new_album():
     rq_album.delete_all(token=env.env_var['USER_2_TOKEN'])
+    rq_album.delete_all(token=env.env_var['USER_1_TOKEN'])
     rq_album.delete_all(token=env.env_var['USER_3_TOKEN'])
 
     new_album = rq_album.create(token=env.env_var['USER_1_TOKEN'])
-    env.env_var["ALBUM_ID_SHARING"]=new_album["album_id"]
+    env.env_var["ALBUM_ID_SOURCE"]=new_album["album_id"]
 
 def test_stow():
-    params = {"album": env.env_var.get("ALBUM_ID_SHARING")}
+    params = {"album": env.env_var["ALBUM_ID_SOURCE"]}
     #insert 5 differents series in album
-    rq_studies.stow(token=env.env_var.get("USER_1_TOKEN"), file_name = "series/test1.dcm", params=params)
-    rq_studies.stow(token=env.env_var.get("USER_1_TOKEN"), file_name = "series/test2.dcm", params=params)
-    rq_studies.stow(token=env.env_var.get("USER_1_TOKEN"), file_name = "series/test3.dcm", params=params)
-    rq_studies.stow(token=env.env_var.get("USER_1_TOKEN"), file_name = "series/test4.dcm", params=params)
-    rq_studies.stow(token=env.env_var.get("USER_1_TOKEN"), file_name = "series/test5.dcm", params=params)
+    rq_studies.stow(token=env.env_var["USER_1_TOKEN"], file_name = "series/test1.dcm", params=params)
+    rq_studies.stow(token=env.env_var["USER_1_TOKEN"], file_name = "series/test2.dcm", params=params)
+    rq_studies.stow(token=env.env_var["USER_1_TOKEN"], file_name = "series/test3.dcm", params=params)
+    rq_studies.stow(token=env.env_var["USER_1_TOKEN"], file_name = "series/test4.dcm", params=params)
+    rq_studies.stow(token=env.env_var["USER_1_TOKEN"], file_name = "series/test4_1.dcm", params=params)
+    rq_studies.stow(token=env.env_var["USER_1_TOKEN"], file_name = "series/test5.dcm", params=params)
 
-def test_album_studies_list():
-    params = {"album": env.env_var.get("ALBUM_ID_SHARING")}
+
+def test_studies_list():
+    params = {"album": env.env_var["ALBUM_ID_SOURCE"]}
     rq_studies.get_list(token=env.env_var.get("USER_1_TOKEN"), params=params, count=5)
 
-#def test_get_events_mutation_when_create_album():
+
+def test_create_new_album_where_share_studies():
+    new_album = rq_album.create(token=env.env_var['USER_2_TOKEN'], data={"name":"share study"})
+    
+    env.env_var["ALBUM_ID_DEST"]=new_album["album_id"]
+    params = {"album": env.env_var["ALBUM_ID_DEST"]}
+    #study is empty
+    rq_studies.get_list(token=env.env_var["USER_2_TOKEN"], params=params, count=0)
+
+
+def test_share_study_in_album():
+    rq_album.add_user(env.env_var["USER_2_TOKEN"], env.env_var["ALBUM_ID_DEST"], env.env_var["USER_1_MAIL"])
+    params = {"album": env.env_var["ALBUM_ID_SOURCE"]}
+    rq_studies.add_in_album(env.env_var["USER_1_TOKEN"], params, env.env_var["STUDY_UID4_1"], env.env_var["ALBUM_ID_DEST"])
+    rq_studies.get_list(token=env.env_var["USER_2_TOKEN"], params={"album": env.env_var["ALBUM_ID_DEST"]}, count=1)
     
