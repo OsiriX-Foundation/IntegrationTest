42bc53eb155a18b0668206919f6073b3603a90ce
gerome12 <gerome.pasquier@gmail.com>
2020-03-12 17:51:03 +0100

    Update test_karnak.py

--- test_karnak.py
+++ test_karnak.py
@@ -115,3 +115,8 @@ def test_clean_kheops():
     rq_album.delete_all(token=env.env_var['USER_2_TOKEN'], user_id=env.env_var['USER_2_MAIL'])
     rq_album.delete_all(token=env.env_var['USER_3_TOKEN'], user_id=env.env_var['USER_3_MAIL'])
     rq_album.delete_all(token=env.env_var['USER_KARNAK_TOKEN'], user_id=env.env_var['USER_KARNAK_MAIL'])
+
+    rq_studies.delete_all_from_inbox(token=env.env_var['USER_1_TOKEN'])
+    rq_studies.delete_all_from_inbox(token=env.env_var['USER_1_TOKEN'])
+    rq_studies.delete_all_from_inbox(token=env.env_var['USER_1_TOKEN'])
+    rq_studies.delete_all_from_inbox(token=env.env_var['USER_1_TOKEN'])
