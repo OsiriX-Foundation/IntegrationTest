307bba35c661dfd8d3814af3ed6dcb9e41260b35
gerome12 <gerome.pasquier@gmail.com>
2020-03-13 10:29:50 +0100

    move webhook request

--- test_webhook.py
+++ test_webhook.py
@@ -10,6 +10,7 @@ import util
 import pprint
 import rq_album
 import rq_studies
+import rq_webhook
 
 def test_init():
     env.initialize()
@@ -30,7 +31,7 @@ def test_new_album_with_2_users():
 
 def test_new_webhook1():
     data = {"url":"https://webhook.site/cb3269f7-7184-4f3b-954b-fdd3c356caab", "name":"name", "secret":"1234", "event":["new_series"]}
-    webhook = util.new_webhook(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), data=data)
+    webhook = rq_webhook.create(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), data=data)
     env.env_var["WEBHOOK_ID"] = webhook["id"]
 
 #def test_new_webhook2():
@@ -45,31 +46,31 @@ def test_new_webhook1():
 
 def test_edit_webhook():
     data = {"name":"new name", "secret":"12346578", "event":["new_series", "new_user"]}
-    webhook = util.edit_webhook(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"), data=data)
+    webhook = rq_webhook.edit(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"), data=data)
 
 def test_get_webhook():
-    webhook = util.get_webhook(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"))
+    webhook = rq_webhook.get(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"))
 
 def test_get_webhooks():
-    webhooks = util.get_webhooks(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"))
+    webhooks = rq_webhook.get_list(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"))
 
 def test_add_user():
     rq_album.add_user(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), user_id=env.env_var.get("USER_2_MAIL"))
     time.sleep(2)
 
 def test_trigger_user_webhook():
-    webhooks = util.trigger_user_webhook(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"), user_id=env.env_var.get("USER_2_MAIL"))
+    webhooks = rq_webhook.trigger_user(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"), user_id=env.env_var.get("USER_2_MAIL"))
     time.sleep(2)
 
 #def test_trigger_series_webhook():
-#    webhooks = util.trigger_series_webhook(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"), series_UID="1.2.3.4", study_UID="1.2.3.4.5.6")
+#    webhooks = rq_webhook.trigger_series(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"), series_UID="1.2.3.4", study_UID="1.2.3.4.5.6")
 #    time.sleep(2)
 
 def test_get_webhook2():
-    webhook = util.get_webhook(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"))
+    webhook = rq_webhook.get(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"))
 
 #def test_delete_webhook():
-#    util.remove_webhook(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"))
+#    rq_webhook.remove(token=env.env_var.get("USER_1_TOKEN"), album_id=env.env_var.get("ALBUM_ID"), webhook_id=env.env_var.get("WEBHOOK_ID"))
 
 #def test_stow():
 #    params = {"album": env.env_var.get("ALBUM_ID")}
