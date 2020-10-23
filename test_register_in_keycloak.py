3cd7ebb977664d438b468ae08f9010629fcee2d7
gerome12 <gerome.pasquier@gmail.com>
2020-10-23 15:36:57 +0200

    Update test_register_in_keycloak.py

--- test_register_in_keycloak.py
+++ test_register_in_keycloak.py
@@ -10,6 +10,7 @@ def test_register_in_keycloak():
     util.add_user_keycloak(admin_username="admin", admin_password="admin", user_name="titi", user_firstname="titi", user_mail="titi@gmail.com", user_username="titi", user_password="titi")
     util.add_user_keycloak(admin_username="admin", admin_password="admin", user_name="toto", user_firstname="toto", user_mail="toto@gmail.com", user_username="toto", user_password="toto")
     util.add_user_keycloak(admin_username="admin", admin_password="admin", user_name="tata", user_firstname="tata", user_mail="tata@gmail.com", user_username="tata", user_password="tata")
+    util.add_user_keycloak(admin_username="admin", admin_password="admin", user_name="karnak", user_firstname="karnak", user_mail="karnak@gmail.com", user_username="karnak", user_password="karnak")
 
 
 def test_get_token():
